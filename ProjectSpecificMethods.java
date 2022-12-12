7cd6fee6fbae9a3544d2ec0d9f0ee445ed4abbce
sumitshelar3 <112796527+sumitshelar3@users.noreply.github.com>
2022-12-12 21:27:34 +0530

    Add files via upload

---
+++ ProjectSpecificMethods.java
@@ -0,0 +1,30 @@
+package baseclass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProjectSpecificMethods extends AbstractTestNGCucumberTests{
+
+	public static WebDriver driver;
+
+	@BeforeMethod
+	public void preCondition() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	}
+
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+	}
+
+}
